ebc020771381246ce9d65da27f765df5868dd48b
YongxinWangGH <umay.yx.wang@gmail.com>
2019-01-13 13:32:45 +1100

    merge showhide module into dropdown module

--- js/dropdown.js
+++ js/dropdown.js
@@ -3,20 +3,32 @@
 
 	function dropdown(elem){
 		var $elem = $(elem),
+			$layer = $elem.find('.dropdown-layer'),
 			activeClass = $elem.data('active') + '-active';
+
+		$layer.showHide();
 
 		$elem.hover(function(){
 			$elem.addClass(activeClass);
+			$layer.showHide('show');
 		},function(){
 			$elem.removeClass(activeClass);
+			$layer.showHide('hide');
 		});
 
 	}
 
+	var defaults = {
+		css3: false,
+		js: false,
+		animation: 'fade'
+	};
+
 	$.fn.extend({
-		dropdown: function(){
+		dropdown: function(option){
 			return this.each(function(){
-				dropdown(this);
+				var options = $.extend({},defaults,option);
+				dropdown(this,options);
 			})
 		}
 	});
